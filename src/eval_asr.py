1afdecb4c99374713fe308190b4036312b7ac76c
choomegan <choomegan@gmail.com>
2025-11-17 17:54:50 +0800

    feat: specify device for eval

--- src/eval_asr.py
+++ src/eval_asr.py
@@ -50,7 +50,7 @@ def main():
     sample_rate = 16000
 
     # Device
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device(f"cuda:{int(config.device)}" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     # Load model & processor
