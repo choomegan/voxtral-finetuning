c654387a81c0004043f9d97160c71dec1160d5f6
choomegan <choomegan@gmail.com>
2025-11-27 17:11:44 +0800

    feat: define language code mapping constants used in training

---
+++ src/utils/constants.py
@@ -0,0 +1,13 @@
+"""
+Script for language mapping constants
+"""
+
+# Mapping for language class weighting.
+SRCLANG2ID = {"zsm": 0, "ind": 1}
+
+# Mapping for 3-letter ISO code to 2-letter code. Used for ASR language specification.
+LANGCODE_MAP = {
+    "en": "en",
+    "zsm": "ms",
+    "ind": "id",
+}
